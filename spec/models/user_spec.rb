0a3aad68cfa5465f6f5c9aa4c570be85abfbf5df
Yuji <weto2kakokako@yahoo.co.jp>
2020-01-11 11:18:24 +0900

    install Factory_bot_rails

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    expect(FactoryBot.create(:user)).to be_valid
+  end
+
+  it "double test" do
+    user = User.new(
+    name: "Elizabeth",
+    email: "elizabeth@gmail.com",
+    password: "password",
+  )
+    expect(user).to be_valid
+  end
+
+  it "truthy true" do
+    expect(1+1).to eq 2
+  end
 end
